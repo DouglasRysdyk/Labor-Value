44febd73dcf057aee2e7ff6c516e0d784c4dc0e9
Doug Rysdyk <dldrysdyk@me.com>
2020-11-22 14:46:57 -0500

    Initial Commit Set up the MainView and MainViewController Deleted defaulted commented out stuff in MainViewController Set up the ResultsView and ResultsViewController

---
+++ Labor-Value/View Controllers/MainViewController.swift
@@ -0,0 +1,15 @@
+//
+//  MainViewController.swift
+//  Labor-Value
+//
+//  Created by Douglas Rysdyk on 11/22/20.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
