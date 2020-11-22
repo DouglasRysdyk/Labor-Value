8e062a0d2a5402bc75cff226f3d8c50959ce08ce
Doug Rysdyk <dldrysdyk@me.com>
2020-11-21 23:38:16 -0500

    Initial Commit

---
+++ Labor-Value/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Labor-Value
+//
+//  Created by Douglas Rysdyk on 11/21/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
