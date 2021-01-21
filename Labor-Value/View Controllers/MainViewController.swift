54db5d28245ed70819632b260308b09c044d7ac8
Doug Rysdyk <dldrysdyk@me.com>
2021-01-21 12:37:33 -0500

    Added an extension to UITextField that adds a Done or Cancel toolbar to the keypad. Added a didSet function to the income and item price textfields that calls the doneCancel extension for those textfields.

--- Labor-Value/View Controllers/MainViewController.swift
+++ Labor-Value/View Controllers/MainViewController.swift
@@ -9,13 +9,16 @@ import UIKit
 
 class MainViewController: UIViewController {
     let theCalculationsModel = Calculations()
-    //MARK: User Defaults draft
-    //TODO: Replace with a call to the UserDefaultsManager class
     
     let savedUserIncomeSuiteDefault = UserDefaultsManager()
     
-    @IBOutlet weak var incomeTextField: UITextField!
-    @IBOutlet weak var itemPriceTextField: UITextField!
+    @IBOutlet weak var incomeTextField: UITextField! {
+        didSet { incomeTextField?.addDoneCancelToolbar() }
+    }
+    
+    @IBOutlet weak var itemPriceTextField: UITextField! {
+        didSet { itemPriceTextField?.addDoneCancelToolbar() }
+    }
     
     @IBOutlet weak var theResult: UILabel!
     
@@ -201,3 +204,26 @@ extension UIViewController {
 }
 
 //User Defaults code based off of https://www.youtube.com/watch?v=XzWBT6lIB3A and https://programmingwithswift.com/how-to-use-userdefaults-suites-with-swift/
+
+//Source (first answer) -- https://stackoverflow.com/questions/38133853/how-to-add-a-return-key-on-a-decimal-pad-in-swift
+extension UITextField {
+    func addDoneCancelToolbar(onDone: (target: Any, action: Selector)? = nil, onCancel: (target: Any, action: Selector)? = nil) {
+        let onCancel = onCancel ?? (target: self, action: #selector(cancelButtonTapped))
+        let onDone = onDone ?? (target: self, action: #selector(doneButtonTapped))
+
+        let toolbar: UIToolbar = UIToolbar()
+        toolbar.barStyle = .default
+        toolbar.items = [
+            UIBarButtonItem(title: "Cancel", style: .plain, target: onCancel.target, action: onCancel.action),
+            UIBarButtonItem(barButtonSystemItem: .flexibleSpace, target: self, action: nil),
+            UIBarButtonItem(title: "Done", style: .done, target: onDone.target, action: onDone.action)
+        ]
+        toolbar.sizeToFit()
+
+        self.inputAccessoryView = toolbar
+    }
+
+    // Default actions:
+    @objc func doneButtonTapped() { self.resignFirstResponder() }
+    @objc func cancelButtonTapped() { self.resignFirstResponder() }
+}
